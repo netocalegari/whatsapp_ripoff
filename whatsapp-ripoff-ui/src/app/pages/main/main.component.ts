23787a201e96a3be8fe7cb5aecbabdfcfaa81e87
netocalegari <netokalegari@gmail.com>
2025-02-27 12:34:52 -0300

    mian interface

---
+++ whatsapp-ripoff-ui/src/app/pages/main/main.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+
+import { ChatListComponent } from "../../components/chat-list/chat-list.component";
+import { ChatResponse } from "../../services/models";
+import { ChatService } from "../../services/services";
+
+@Component({
+  selector: "app-main",
+  imports: [ChatListComponent],
+  templateUrl: "./main.component.html",
+  styleUrl: "./main.component.scss",
+})
+export class MainComponent implements OnInit {
+  chats: Array<ChatResponse> = [];
+
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.getAllChats();
+  }
+
+  private getAllChats() {
+    this.chatService.getChatsByReceiver().subscribe({
+      next: (res) => {
+        this.chats = res;
+      },
+    });
+  }
+}
