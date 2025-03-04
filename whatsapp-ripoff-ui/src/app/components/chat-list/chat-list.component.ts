ddfe0fb17418c21ce983476af8382f081b85867f
netocalegari <netokalegari@gmail.com>
2025-03-04 11:59:48 -0300

    implementing messages list ui

--- whatsapp-ripoff-ui/src/app/components/chat-list/chat-list.component.ts
+++ whatsapp-ripoff-ui/src/app/components/chat-list/chat-list.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from "@angular/common";
-import { Component, input, InputSignal } from "@angular/core";
+import { Component, input, InputSignal, output } from "@angular/core";
 
 import { ChatResponse, UserResponse } from "../../services/models";
 import { UserService } from "../../services/services";
@@ -14,6 +14,7 @@ export class ChatListComponent {
   chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
   searchNewContact = false;
   contacts: Array<UserResponse> = [];
+  chatSelected = output<ChatResponse>();
 
   constructor(private userService: UserService) {}
 
@@ -26,7 +27,10 @@ export class ChatListComponent {
     });
   }
 
-  chatClicked(chat: ChatResponse) {}
+  chatClicked(chat: ChatResponse) {
+    console.log("Chat clicked:", chat); // Adicione este log
+    this.chatSelected.emit(chat);
+  }
 
   wrapMessage(lastMessage: string | undefined): string {
     if (lastMessage && lastMessage.length <= 20) return lastMessage;
--- whatsapp-ripoff-ui/src/app/pages/main/main.component.ts
+++ whatsapp-ripoff-ui/src/app/pages/main/main.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 
 import { ChatListComponent } from "../../components/chat-list/chat-list.component";
-import { ChatResponse } from "../../services/models";
-import { ChatService } from "../../services/services";
+import { ChatResponse, MessageResponse } from "../../services/models";
+import { ChatService, MessageService } from "../../services/services";
 import { KeycloakService } from "../../utils/keycloak/keycloak.service";
 
 @Component({
@@ -13,10 +13,13 @@ import { KeycloakService } from "../../utils/keycloak/keycloak.service";
 })
 export class MainComponent implements OnInit {
   chats: Array<ChatResponse> = [];
+  selectedChat: ChatResponse = {};
+  chatMessages: Array<MessageResponse> = [];
 
   constructor(
     private chatService: ChatService,
-    private keyCloakSerice: KeycloakService
+    private keyCloakSerice: KeycloakService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
@@ -27,6 +30,7 @@ export class MainComponent implements OnInit {
     this.chatService.getChatsByReceiver().subscribe({
       next: (res) => {
         this.chats = res;
+        console.log("Chats loaded:", this.chats); // Adicione este log
       },
     });
   }
@@ -36,5 +40,32 @@ export class MainComponent implements OnInit {
   }
   userProfile() {
     this.keyCloakSerice.accountManagement();
+  }
+
+  chatSelected(chatResponse: ChatResponse) {
+    console.log("Chat selected:", chatResponse); // Adicione este log
+
+    this.selectedChat = chatResponse;
+    this.getAllChatMessages(chatResponse.id as string);
+    this.setMessagesToSeen();
+    // this.selectedChat.unreadCount = 0;
+  }
+
+  getAllChatMessages(chatId: string) {
+    this.messageService
+      .getMessages({
+        "chat-id": chatId,
+      })
+      .subscribe({
+        next: (messages) => {
+          this.chatMessages = messages;
+        },
+      });
+  }
+
+  setMessagesToSeen() {}
+
+  isSelfMessage(message: MessageResponse) {
+    return message.senderId === this.keyCloakSerice.userId;
   }
 }
